eed896112b9b8633efd1f01c56bdca2d477b95cc
Федя Чашкин <fbchashkin@edu.hse.ru>
2025-05-08 15:51:38 +0300

    Добавление базового функционала бота

---
+++ main.py
@@ -0,0 +1,12 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from config import BOT_TOKEN
+
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+
+app = ApplicationBuilder().token(BOT_TOKEN).build()
+
+app.add_handler(CommandHandler("start", hello))
+
+app.run_polling()
