21f1cb35290facf978a0e0c6560d51ecf05bca9f
Федя Чашкин <fbchashkin@edu.hse.ru>
2025-05-10 09:35:05 +0300

    Добавление кнопки приветствия

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, CallbackQueryHandler
 from config import BOT_TOKEN
 
-async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:  
+    button = [[InlineKeyboardButton("Поздороваться", callback_data="say_hello")]]
+    await update.message.reply_text(  
+        text="Привет!",
+        reply_markup=InlineKeyboardMarkup(button) 
+    )
+
+async def say_hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    query = update.callback_query
+    await query.answer()
+    await query.message.reply_text(text=f"Привет, {query.from_user.first_name}!")
 
 app = ApplicationBuilder().token(BOT_TOKEN).build()
-
-app.add_handler(CommandHandler("start", hello))
-
+app.add_handler(CommandHandler("start", start))
+app.add_handler(CallbackQueryHandler(say_hello))
 app.run_polling()
