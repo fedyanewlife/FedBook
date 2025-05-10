0056077d765e7f0ffe8cf283877d3fa3316ce098
Федя Чашкин <fbchashkin@edu.hse.ru>
2025-05-10 10:06:04 +0300

    Добавление вывода книг по кнопке

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, CallbackQueryHandler
 from config import BOT_TOKEN
+from books import BOOKS_LIST
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:  
-    button = [[InlineKeyboardButton("Поздороваться", callback_data="say_hello")]]
+    button = [[InlineKeyboardButton("Поздороваться", callback_data="print_books")]]
     await update.message.reply_text(  
-        text="Привет!",
+        text=f"Привет, {update.message.from_user.first_name}!",
         reply_markup=InlineKeyboardMarkup(button) 
     )
 
-async def say_hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def print_books(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     query = update.callback_query
     await query.answer()
-    await query.message.reply_text(text=f"Привет, {query.from_user.first_name}!")
+    text = (
+        "Список прочитанных книг:\n\n"
+        + "\n".join(BOOKS_LIST)
+    )
+    await query.message.reply_text(text=text)
 
 app = ApplicationBuilder().token(BOT_TOKEN).build()
 app.add_handler(CommandHandler("start", start))
-app.add_handler(CallbackQueryHandler(say_hello))
+app.add_handler(CallbackQueryHandler(print_books))
 app.run_polling()
